9352d54c14b09b0f5b64a49ad60d9d46840999e1
jjfly10086 <824576571@qq.com>
2016-12-19 12:38:50 +0800

    工具类异常抛出 时间 2016年12月19日12:38:28

--- src/main/java/com/demo/utils/SerializeUtils.java
+++ src/main/java/com/demo/utils/SerializeUtils.java
@@ -2,40 +2,29 @@ package com.demo.utils;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 /**
  * 序列化工具类
+ * 
  * @author admin
  */
 public class SerializeUtils {
-	public static byte[] serialize(Object object) {
+	public static byte[] serialize(Object object) throws IOException {
 		ObjectOutputStream oos = null;
-		ByteArrayOutputStream baos = null;
-		try {
-			// 
-			baos = new ByteArrayOutputStream();
-			oos = new ObjectOutputStream(baos);
-			oos.writeObject(object);
-			byte[] bytes = baos.toByteArray();
-			return bytes;
-		} catch (Exception e) {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		oos = new ObjectOutputStream(baos);
+		oos.writeObject(object);
+		byte[] bytes = baos.toByteArray();
+		return bytes;
 
-		}
-		return null;
 	}
 
-	public static Object unserialize(byte[] bytes) {
-		ByteArrayInputStream bais = null;
-		try {
-			// 
-			bais = new ByteArrayInputStream(bytes);
-			ObjectInputStream ois = new ObjectInputStream(bais);
-			return ois.readObject();
-		} catch (Exception e) {
-
-		}
-		return null;
+	public static Object unserialize(byte[] bytes) throws IOException, ClassNotFoundException {
+		ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+		ObjectInputStream ois = new ObjectInputStream(bais);
+		return ois.readObject();
 	}
 }

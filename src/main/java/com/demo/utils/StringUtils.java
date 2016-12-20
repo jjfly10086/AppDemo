3b7a2eae82d8e3e1afb726d3d541aff30c17eb73
jjfly10086 <824576571@qq.com>
2016-12-20 10:11:18 +0800

    StringUtils工具类

--- src/main/java/com/demo/utils/StringUtils.java
+++ src/main/java/com/demo/utils/StringUtils.java
@@ -1,4 +1,7 @@
 package com.demo.utils;
+
+import com.demo.bean.UserBean;
+
 /**
  * String字符串工具类
  * @author admin
@@ -6,25 +9,32 @@ package com.demo.utils;
  */
 public class StringUtils {
 	/**
-	 * 判断字符串为空，对象为空和空字符串
+	 * 判断对象是否为空，可判断空对象和空字符串
 	 * @param str
 	 * @return
 	 */
-	public static boolean isEmpty(String... str){
-		if(null == str){
+	public static boolean isEmpty(Object... obj){
+		if(null == obj){
 			return true;
 		}
-		for(String s : str){
-			if(null == s || "".equals(s)){
+		for(Object o : obj){
+			if(null == o){
 				return true;
+			}
+			if(o instanceof String){
+				if("".equals(o)){
+					return true;
+				}
 			}
 		}
 		return false;
 	}
 	public static void main(String[] args) {
 		String s = "1";
-		String s1 = "1";
-		String s2 = "";
-		System.out.println(isEmpty(s,s1,s2));
+		String s1 = "";
+		String s2 = "2";
+		Float a = 22f;
+		UserBean user = new UserBean();
+		System.out.println(isEmpty(s,s1,s2,user,a));
 	}
 }
